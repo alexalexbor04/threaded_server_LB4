7a311e730f46d086e78cde77e2872d912fc24c57
mishaklok <vbif.hfcjd@gmail.com>
2024-05-01 21:14:26 +0300

    ctrl+c ctrl+v client from echo server

--- client.py
+++ client.py
@@ -1,16 +1,30 @@
 import socket
-from time import sleep
 
-sock = socket.socket()
-sock.setblocking(1)
-sock.connect(('10.38.165.12', 9090))
+IP = input('Введите ip адрес для подключения (если ничего ен вводить, значение по умолчанию - 127.0.0.1): ')
+if IP == None:
+    IP = '127.0.0.1'
+    
+port = int(input('Введите порт для подключения (если ничего ен вводить, значение по умолчанию - 12345): '))
+if port == None:
+    port = 12345
 
-#msg = input()
-msg = "Hi!"
-sock.send(msg.encode())
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_socket.connect((IP, port))
+print(f'Соединение с {IP}:{port} успешно установлено')
 
-data = sock.recv(1024)
+try:
+    while True:
+        message_from_server = client_socket.recv(1024).decode()
+        print(f'Получено: {message_from_server}')
 
-sock.close()
+        message_from_client = input('Введите текст для отправки: ')
 
-print(data.decode())
+        client_socket.sendall(message_from_client.encode())
+        print(f'Отправлено: {message_from_client}')
+
+        if message_from_client == 'exit':
+            break
+except:
+    print('Возникла непредвиденная ошибка, попробуйте снова.')
+    client_socket.close()
+client_socket.close()
