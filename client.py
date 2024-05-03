3995e10685e8ce17cce71c8386ad24b5970b371a
Sasha <alexalexbor04@gmail.com>
2024-05-03 03:41:35 +0300

    н***********я не работает мне пофиг кто увидит этот коммит

--- client.py
+++ client.py
@@ -1,32 +1,56 @@
 import socket
+from threading import Thread
 
-IP = input('Введите ip адрес для подключения (если ничего ен вводить, значение по умолчанию - 127.0.0.1): ')
-if IP == None:
-    IP = '127.0.0.1'
+connection_alive = True
 
-port = input('Введите порт для подключения (если ничего не вводить, значение по умолчанию - 12345): ')
-if port == '':
-    port = 12345
-else:
-    port = int(port)
+def check_message(message=''):
+    a = input(message)
+    if a in ['exit', '/stop']:
+        exit()
+    return a
 
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client_socket.connect((IP, port))
-print(f'Соединение с {IP}:{port} успешно установлено')
+def receive_messages():
+    global connection_alive
+    while True:
+        try:
+            data = client_socket.recv(1024).decode('utf-8')
+            print(data)
+        except (ConnectionRefusedError, ConnectionAbortedError, ConnectionResetError) as error:
+            connection_alive = False
+            print(error)
+            break
+
+
+def send_text(conn, message):
+    message = message.encode('utf-8')
+    conn.send(message)
 
 try:
+    print("Для использования настроек по умолчанию ничего не вводите")
+    IP = input('Введите ip адрес для подключения (если ничего ен вводить, значение по умолчанию - 127.0.0.1): ')
+    if IP == '':
+        IP = '127.0.0.1'
+    port = input('Введите порт для подключения (если ничего не вводить, значение по умолчанию - 12345): ')
+    if port == '':
+        port = 12345
+    else:
+        port = int(port)
+except ValueError:
+    print("Неверно указан порт! Попробуйте еще раз!")
+try:
+    client_socket = socket.socket()
+    client_socket.connect((IP, port))
+    print(f'Соединение с {IP}:{port} успешно установлено')
+except (socket.gaierror, ConnectionRefusedError) as error:
+    print(f"Не удается подключиться к {IP}:{port} ({error})!")
+
+Thread(target=receive_messages, daemon=True).start()
+
+while True:
     while True:
-        message_from_server = client_socket.recv(1024).decode()
-        print(f'Получено: {message_from_server}')
-
-        message_from_client = input('Введите текст для отправки: ')
-
-        client_socket.sendall(message_from_client.encode())
-        print(f'Отправлено: {message_from_client}')
-
-        if message_from_client == 'exit':
+        message_from_server = check_message()
+        if connection_alive:
+            client_socket.sendall(message_from_server.encode())
+        else:
+            client_socket.close()
             break
-except:
-    print('Возникла непредвиденная ошибка, попробуйте снова.')
-    client_socket.close()
-client_socket.close()
